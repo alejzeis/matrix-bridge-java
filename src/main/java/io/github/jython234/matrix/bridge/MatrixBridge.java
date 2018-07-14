7d27a2b72c7c9cc268ee57b9cae2e216bf5fe8b1
jython234 <jython234@users.noreply.github.com>
2018-07-14 14:41:46 -0500

    Update to use matrix-appservice-java version 1.2.4 (fix queue size limit)

--- src/main/java/io/github/jython234/matrix/bridge/MatrixBridge.java
+++ src/main/java/io/github/jython234/matrix/bridge/MatrixBridge.java
@@ -56,7 +56,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public abstract class MatrixBridge {
     public static final String SOFTWARE = "matrix-bridge-java";
-    public static final String VERSION = "1.2.3-SNAPSHOT";
+    public static final String VERSION = "1.2.4-SNAPSHOT";
 
     private MatrixAppservice appservice;
 
